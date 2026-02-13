b4e16412f33912ed058995c57e5a08d50c82dfaf
nacho <ignacio.farias.v@usach.cl>
2026-02-13 19:42:46 -0300

    feat(domain): add DomainError base class and Pokemon error hierarchy

---
+++ client/src/domain/errors/DomainError.ts
@@ -0,0 +1,10 @@
+export abstract class DomainError extends Error {
+  constructor(
+    message: string,
+    public readonly code: string
+  ) {
+    super(message)
+    this.name = this.constructor.name
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
---
+++ client/src/domain/errors/index.ts
@@ -0,0 +1,7 @@
+export { DomainError } from './DomainError'
+export {
+  InvalidPokemonTypeError,
+  InvalidStatsError,
+  InvalidPhysicalMeasurementError,
+  InvalidSpriteUrlError,
+} from './pokemon.errors'
